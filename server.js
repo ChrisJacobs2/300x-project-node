6885613ad14852fa56e09020e71cb83a27968e82
ChrisJacobs2 <cmjacobs@uncg.edu>
2024-04-16 16:21:45 -0400

    installed nodemon and made server.js. No routes yet

---
+++ server.js
@@ -0,0 +1,30 @@
+"use strict";
+const express = require("express");
+const app = express();
+const path = require("path");
+const multer = require("multer");
+app.use(multer().none());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+//views
+// USING EJS ENGINE
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//routes
+// const productsRouter = require("./routes/products.route");
+// app.use("/products", productsRouter);
+
+
+//Home Page
+app.get("/", (req, res) => {
+  res.render("index", { title: 'Home Page', user: req.user });
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log("App listening at http://localhost:" + PORT);
+});
