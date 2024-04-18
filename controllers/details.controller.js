f8869797f634b50a023a989c2314e4c954c22aae
Will Hutcheon <willhutcheon@gmail.com>
2024-04-18 13:01:55 -0400

    details works for endpoints formatted like this: http://localhost:8000/details/item/1

--- controllers/details.controller.js
+++ controllers/details.controller.js
@@ -10,88 +10,6 @@ app.use(express.json());
 
 const model = require("../models/details.model");
 
-/* async function getOneById(req, res) {
-    try {
-        const productId = req.params.id;
-        const product = await detailsModel.getOneById(productId);
-        if (!product) {
-            return res.status(404).json({ error: "Product not found" });
-        }
-        res.render("details", { product });
-    } catch (error) {
-        console.error("Error fetching product details:", error);
-        res.status(500).json({ error: "Internal server error" });
-    }
-} */
-
-/* function getOneById(req, res, next) {
-    let id = req.params.id;
-    try {
-        let meal = model.getOneById(id);
-        res.render("item-details", { meal: meal, title: 'Meal #' + id });
-        //res.json(model.getOneById(req.params.id));
-    } catch (err) {
-        console.error("Error while getting menu ", err.message);
-        next(err);
-    }
-} */
-
-/* function getOneById(req, res, next) {
-    let id = req.params.id;
-    try {
-        let meal = model.getOneById(id);
-        res.render("details", { meal: meal, title: 'Meal #' + id });
-        //res.json(model.getOneById(req.params.id));
-    } catch (err) {
-        console.error("Error while getting menu ", err.message);
-        next(err);
-    }
-} */
-
-/* async function getOneById(req, res, next) {
-    let id = req.params.id;
-    try {
-        let product = await model.getOneById(id); // Await the result
-        if (!product) {
-            return res.status(404).json({ error: "Product not found" });
-        }
-        res.render("details", { product: product }); // Pass product as an object
-    } catch (err) {
-        console.error("Error while getting product details ", err.message);
-        next(err);
-    }
-} */
-
-/* function getOneById(req, res, next) {
-    let id = req.params.id;
-    try {
-        let product = model.getOneById(id); // Await the result
-        if (!product) {
-            return res.status(404).json({ error: "Product not found" });
-        }
-        res.render("details", { product: product }); // Pass product as an object
-    } catch (err) {
-        console.error("Error while getting product details ", err.message);
-        next(err);
-    }
-}
- */
-
-/* function getOneById(req, res, next) {
-    let id = req.params.id;
-    try {
-        let product = model.getOneById(id); // Await the result
-        console.log("Product retrieved:", product); // Log the retrieved product
-        if (!product) {
-            return res.status(404).json({ error: "Product not found" });
-        }
-        res.render("details", { product: product }); // Pass product as an object
-    } catch (err) {
-        console.error("Error while getting product details ", err.message);
-        next(err);
-    }
-} */
-
 async function getOneById(req, res, next) {
     let id = req.params.id;
     try {
@@ -107,9 +25,6 @@ async function getOneById(req, res, next) {
     }
 }
 
-
-
 module.exports = {
     getOneById,
 };
-
