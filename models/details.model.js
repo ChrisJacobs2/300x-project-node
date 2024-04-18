7c901bd0fb6fe240a01668652c021d192c026f1e
Will Hutcheon <willhutcheon@gmail.com>
2024-04-18 02:13:19 -0400

    ):

--- models/details.model.js
+++ models/details.model.js
@@ -3,7 +3,7 @@ const db = require("../models/db-conn");
 const path = require("path");
 
 async function getOneById(id) {
-    let sql = "SELECT * FROM MENU WHERE id = ?;";
+    let sql = "SELECT * FROM Products WHERE id = ?;";
     const item = await db.get(sql, id);
     return item;
 }
