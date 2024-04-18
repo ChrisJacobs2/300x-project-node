850b5c049a5324656e471ee161ad10f81301cef1
Will Hutcheon <willhutcheon@gmail.com>
2024-04-18 02:34:15 -0400

    new error

--- controllers/details.controller.js
+++ controllers/details.controller.js
@@ -24,6 +24,18 @@ const model = require("../models/details.model");
     }
 } */
 
+/* function getOneById(req, res, next) {
+    let id = req.params.id;
+    try {
+        let meal = model.getOneById(id);
+        res.render("item-details", { meal: meal, title: 'Meal #' + id });
+        //res.json(model.getOneById(req.params.id));
+    } catch (err) {
+        console.error("Error while getting menu ", err.message);
+        next(err);
+    }
+} */
+
 function getOneById(req, res, next) {
     let id = req.params.id;
     try {
--- models/details.model.js
+++ models/details.model.js
@@ -3,7 +3,7 @@ const db = require("../models/db-conn");
 const path = require("path");
 
 async function getOneById(id) {
-    let sql = "SELECT * FROM Products WHERE id = ?;";
+    let sql = "SELECT * FROM Products WHERE productID = ?;";
     const item = await db.get(sql, id);
     return item;
 }
