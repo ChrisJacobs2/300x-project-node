ebd6d85ca9dc087342846fd2fcbca2baee1be839
ChrisJacobs2 <cmjacobs@uncg.edu>
2024-04-17 23:39:33 -0400

    just added a little comment

--- routes/products.route.js
+++ routes/products.route.js
@@ -4,6 +4,20 @@ const router = express.Router();
 
 const productscontroller = require("../controllers/products.controller");
 
+// Create a cookie for the user if it doesn't have one yet. This is used to identify them.
+// If we end up using google auth, I'll eat 50 hot dogs in one sitting. 
+
+// app.use((req, res, next) => {
+//     const userId = req.cookies.userId;
+//     if (!userId) {
+//       const newUserId = generateUniqueUserId();
+//       res.cookie('userId', newUserId, { maxAge: 7 * 24 * 60 * 60 * 1000, httpOnly: true }); // Set maxAge for 7 days
+//     }
+//     next(); // Pass control to the next middleware or route
+//   });
+
+// We probably won't need this for the products router. Hi Sunny!
+
 router.get("/all", productscontroller.getAll);
 router.get("/category/:category", productscontroller.getAllByCategory);
 router.get("/item/:id", productscontroller.getOneById);
@@ -11,6 +25,8 @@ router.post("/new", productscontroller.createNew);
 router.get("/search", productscontroller.searchByName);
 router.delete("/delete/:id", productscontroller.deleteById);
 router.put("/update/:id", productscontroller.update)
+
+
 
 
 
