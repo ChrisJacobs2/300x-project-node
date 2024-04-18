d160d52e4cf1effd80414c6bcadbbe91976e1b5f
Will Hutcheon <willhutcheon@gmail.com>
2024-04-18 00:59:18 -0400

    modified get one by id function for details mvc

--- controllers/details.controller.js
+++ controllers/details.controller.js
@@ -10,15 +10,17 @@ app.use(express.json());
 
 const model = require("../models/details.model");
 
-function getOneById(req, res, next) {
-    let id = req.params.id;
+async function getOneById(req, res) {
     try {
-        let meal = model.getOneById(id);
-        res.render("item-details", { meal: meal, title: 'Meal #' + id });
-        //res.json(model.getOneById(req.params.id));
-    } catch (err) {
-        console.error("Error while getting menu ", err.message);
-        next(err);
+        const productId = req.params.id;
+        const product = await productsModel.getOneById(productId);
+        if (!product) {
+            return res.status(404).json({ error: "Product not found" });
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        console.error("Error fetching product details:", error);
+        res.status(500).json({ error: "Internal server error" });
     }
 }
 
