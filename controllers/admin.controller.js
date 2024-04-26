a1a4c8a0518146344f46b5d8260824633cffc59c
Will Hutcheon <willhutcheon@gmail.com>
2024-04-26 11:57:24 -0400

    cleaned up comments

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -14,46 +14,6 @@ function renderAddProductPage(req, res) {
     res.render("admin");
 }
 
-/* function adminCreateNew(req, res, next) {
-    let productID = parseInt(req.body.productID);
-    let productName = req.body.productName;
-    let productDescription = req.body.productDescription;
-    let productImageURL = req.body.productImageURL;
-    let productPrice = parseFloat(req.body.productPrice);
-    let categoryID = parseInt(req.body.categoryID);
-    let featuredProduct = req.body.featuredProduct === 'true' || req.body.featuredProduct === true ? 1 : 0;
-    if (productID && productName && productDescription && productImageURL && productPrice && categoryID && featuredProduct) {
-        let params = [productID, productName, productDescription, productImageURL, productPrice, categoryID, featuredProduct];
-        try {
-            model.adminCreateNew(params);
-            // res.redirect('/menu/all');
-        } catch (err) {
-            console.error("Error while creating menu ", err.message);
-            next(err);
-        }
-    }
-} */
-
-/* async function adminCreateNew(req, res, next) {
-    let productID = parseInt(req.body.productID);
-    let productName = req.body.productName;
-    let productDescription = req.body.productDescription;
-    let productImageURL = req.body.productImageURL;
-    let productPrice = parseFloat(req.body.productPrice);
-    let categoryID = parseInt(req.body.categoryID);
-    let featuredProduct = req.body.featuredProduct === 'true' || req.body.featuredProduct === true ? 1 : 0;
-    if (productID && productName && productDescription && productImageURL && productPrice && categoryID && featuredProduct) {
-        let params = [productID, productName, productDescription, productImageURL, productPrice, categoryID, featuredProduct];
-        try {
-            await model.adminCreateNew(params);
-            //res.redirect('/menu/all');
-        } catch (err) {
-            console.error("Error while creating menu ", err.message);
-            next(err);
-        }
-    }
-} */
-
 async function adminCreateNew(req, res, next) {
     let productID = parseInt(req.body.productID);
     let productName = req.body.productName;
